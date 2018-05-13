8324b258a8fc32f12dc4589fa93e619c68528284
palficsabapeter <palficsabapeter@gmail.com>
2018-05-13 16:41:20 +0200

    GameOver bug megcsinalva

--- src/controller/Game.java
+++ src/controller/Game.java
@@ -424,6 +424,7 @@ public class Game implements ControllerEventListener {
             pushSource.stepOn(temp);
         }
 
+        phantom.setActCell(null);
         phantom.enable();
         phantom.die();
 
